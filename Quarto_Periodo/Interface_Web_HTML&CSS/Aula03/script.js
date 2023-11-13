0e2578546d3b4c8df9d9ff152fcfa8c21ff05dd2
Alysson <alyssonjfranca@gmail.com>
2023-11-13 19:50:59 -0300

    Adiciona aula 04 e atualizacoes

--- Quarto_Periodo/Interface_Web_HTML&CSS/Aula03/script.js
+++ Quarto_Periodo/Interface_Web_HTML&CSS/Aula03/script.js
@@ -4,7 +4,28 @@ let input1 = document.getElementById('input1');
 function addTarefa(){
     const texto = input1.value;
     let novoElemento = document.createElement("li");
-    novoElemento.innerHTML = texto;
+    novoElemento.innerHTML = `<p>${texto}</p>
+                                <button onclick='excluirTarefa(this)'>Excluir</button>
+                                <button onclick='concluirTarefa(this)'>Concluir</button>
+                                <button onclick='editarTarefa(this)'>Editar</button>`;
+    listaTarefas.appendChild(novoElemento);
+    input1.value = "";
+}
+
+function excluirTarefa(button) {
+    alert("Item Excluido");
+    listaTarefas.removeChild(button.parentElement);
+}
+
+function concluirTarefa(button) {
+    alert("Item Concluido");
+    listaTarefas(button.parentElement).style.color = "#16f216";
+}
+
+function editarTarefa(button) {
+    alert("Coloque o valor desejado na area de adicionar e pressioner editar");
+        const texto = input1.value;
+    let novoElemento = document.innerHTML = `<p>${texto}</p>`;
     listaTarefas.appendChild(novoElemento);
     input1.value = "";
 }
