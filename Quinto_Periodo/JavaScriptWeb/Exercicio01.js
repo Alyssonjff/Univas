2f38e796c31a71a4119a8169d93254ee7e9802e6
Alyssonjff <alyssonjfranca@gmail.com>
2024-02-28 18:25:46 -0300

    first exercice

---
+++ Quinto_Periodo/JavaScriptWeb/Exercicio01.js
@@ -0,0 +1,15 @@
+const nome = 'Alysson';
+const idade = 20;
+console.log(`nome - ` + nome + ` e idade - ` +idade);
+
+const X = 5; 
+const Y = 4;
+console.log(X+Y);
+console.log(X-Y);
+console.log(X*Y);
+console.log(X/Y);
+
+const palavra1 = `Ruina`;
+const palavra2 = `Perdicao`;
+var juncao = str.concat(palavra1,palavra2);
+console.log(juncao);
