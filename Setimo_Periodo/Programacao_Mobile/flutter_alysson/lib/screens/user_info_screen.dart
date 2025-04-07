8839af930c22f1e11d90d330ff9046f558d833d1
Alyssonjff <alyssonjfranca@gmail.com>
2025-04-07 17:26:29 -0300

    update user_screen

--- Setimo_Periodo/Programacao_Mobile/flutter_alysson/lib/screens/user_info_screen.dart
+++ Setimo_Periodo/Programacao_Mobile/flutter_alysson/lib/screens/user_info_screen.dart
@@ -28,24 +28,56 @@ class UserInfoScreen extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Nome: ${user['name']}',
-              style: TextStyle(color: Colors.white, fontSize: 18),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Email: ${user['email']}',
-              style: TextStyle(color: Colors.white, fontSize: 18),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Função: ${user['role']}',
-              style: TextStyle(color: Colors.white, fontSize: 18),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/icons/avatar.jpg'),
+                backgroundColor: Colors.transparent,
+              ),
+              SizedBox(height: 20),
+
+              Card(
+                color: Colors.white,
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Text('${user['name']}', style: TextStyle(fontSize: 18)),
+                      SizedBox(height: 10),
+                      Text('${user['email']}', style: TextStyle(fontSize: 18)),
+                      SizedBox(height: 10),
+                      Text('${user['role']}', style: TextStyle(fontSize: 18)),
+                    ],
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 30),
+
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/login');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFD3D3D3),
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  minimumSize: Size(double.infinity, 60),
+                ),
+                child: Text(
+                  'Logout',
+                  style: TextStyle(fontSize: 18, fontFamily: 'Algerian'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: Stack(
@@ -57,16 +89,6 @@ class UserInfoScreen extends StatelessWidget {
               icon: Icon(Icons.arrow_back, size: 30, color: Colors.white),
               onPressed: () {
                 Navigator.pop(context);
-              },
-            ),
-          ),
-          Positioned(
-            bottom: 20,
-            right: 20,
-            child: IconButton(
-              icon: Icon(Icons.home, size: 40, color: Colors.white),
-              onPressed: () {
-                Navigator.pushNamed(context, '/');
               },
             ),
           ),
